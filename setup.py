93ea4e871062636683e071c46a0100d51a9288e1
masterfloss <carlos.costa@acm.org>
2024-11-22 13:22:50 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='module_name',  # Replace with your module's name
+    version='1.0.0',
+    py_modules=['greetings'],  # The name of your Python file (without .py)
+    description='Module examplifying',
+    author='Carlos J. Costa',
+    author_email='your_email@example.com',
+    url='https://github.com/cjcosta/a',
+)
