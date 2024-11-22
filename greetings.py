398309f8ca94dfc391d24ce451ff5661ede0fc95
masterfloss <carlos.costa@acm.org>
2024-11-22 11:18:37 +0000

    Create greetings.py

---
+++ greetings.py
@@ -0,0 +1,10 @@
+def say_hello(name):
+    """Greet someone by name."""
+    return f"Hello, {name}!"
+
+def say_goodbye(name):
+    """Bid farewell to someone by name."""
+    return f"Goodbye, {name}!"
+
+# A variable in the module
+DEFAULT_NAME = "World"
